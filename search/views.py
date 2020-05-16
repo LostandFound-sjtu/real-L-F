4d89dfb8321f778a480f7df7b90ea02824d1b214
Yang Xu <57709194+radiocat-bit@users.noreply.github.com>
2020-05-16 16:35:39 +0800

    修改代码使之兼容wzd的tag类

--- search/views.py
+++ search/views.py
@@ -11,13 +11,14 @@ def multi_search(request, *args, **kwargs):
     request_path = request.path
     real_class_list = ['F', 'L']
     class_list = ['0', '1', '2']
-    tag_list = ['0', '1', '2', '3']
+    tag_list = ['0', '1', '2', '3', '4']
     class_id = kwargs.get('class_id')
     tag_id = kwargs.get('tag_id')
     item_list = []
     if class_id == '0' and tag_id != '0':
         print('s1')
-        item_list = models.Item.objects.filter(tags=str(int(tag_id)-1))
+        item_list = models.Item.objects.filter(tag=tag_list[int(tag_id)])
+
     elif class_id !='0' and tag_id == '0':
         print('s2')
         item_list = models.Item.objects.filter(category=real_class_list[int(class_id) - 1])
@@ -27,7 +28,7 @@ def multi_search(request, *args, **kwargs):
     elif class_id !='0' and tag_id !='0':
         print('s4')
         item_list = models.Item.objects.filter(Q(category__icontains=real_class_list[int(class_id) - 1]) &
-                                               Q(tags__icontains=str(int(tag_id)-1))
+                                               Q(tag=tag_list[int(tag_id)])
                                                )
     return render(request, 'multi_search.html',{'class_list':class_list,
                                                 'tag_list':tag_list,
