11e54da2d2a663e924ebc0bc38216acffc4880d9
wang-weize <45849124+wang-weize@users.noreply.github.com>
2020-05-26 17:14:18 +0800

    Add files via upload

--- item/tests.py
+++ item/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from item.models import Item
+from tag.models import Tag
+from home.models import MyUser
+import unittest
+from django.test import Client
 
 # Create your tests here.
+
+class ItemTest(TestCase):
+
+    def setUp(self):
+        #c = Client()
+
+        #c.login(username='admin', password='sjtu-admin')
+        MyUser.objects.create_user(username='hh', phone_number='13578946200', gender='M', email='1293018434@qq.com',password='asdad')
+        Tag.objects.create(name = '手机')
+        Item.objects.create(status='test', user = MyUser.objects.get(username='hh'), name='phone', 
+                            
+                            category = 'F',
+                            location = 'nn',
+                            phone_number = '111',
+                            mail_address = '1932798582@qq.com',
+                            image = ' ',
+                            identification_mark = 'aa',
+                            secret_information = 'aa',
+                            tag = Tag.objects.get(name = '手机')
+                            )
+                      
+
+    def test_email_successful(self):
+        item1 = Item.objects.get(name='phone')
+        mail = item1.get_mail()
+        self.assertEqual(mail, '1932798582@qq.com')
+        
