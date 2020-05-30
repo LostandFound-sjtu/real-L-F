1c6d5aea1ed0e232174e04572b2f4b730eab680b
qiushi <dreamtale.sjtu@gmail.com>
2020-05-30 12:27:31 +0800

    111

--- item/models.py
+++ item/models.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 
 
 class Item(models.Model):
-    status = models.CharField(max_length=50, verbose_name='目前状态',default='')
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, verbose_name='创建者')
     name = models.CharField(max_length=50, verbose_name='名称')
     CATEGORY_CHOICES = (
